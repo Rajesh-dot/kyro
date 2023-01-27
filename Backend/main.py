ac8774e97f264dc645cd96e9db7b42758969599f
Rajesh-dot <57223067+Rajesh-dot@users.noreply.github.com>
2023-01-27 11:51:08 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,12 @@
 import uvicorn
 
+import importlib.util
+
+spec = importlib.util.find_spec("redis_om")
+if spec is None:
+    print("Module not found.")
+else:
+    print("Module found.")
+
 if __name__ == "__main__":
     uvicorn.run("Users.main:app", host="0.0.0.0", port=8000, reload=True)
