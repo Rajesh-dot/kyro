ae773733945409eed2640c7a5cc331edb355d45a
Rajesh-dot <57223067+Rajesh-dot@users.noreply.github.com>
2023-01-28 20:17:17 +0530

    Update main.py
    - CORS fix

--- Backend/Users/main.py
+++ Backend/Users/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # config
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['*'],
     allow_methods=['*'],
     allow_headers=['*']
 )
